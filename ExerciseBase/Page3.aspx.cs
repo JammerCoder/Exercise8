663c789985270e1c15f20fadc53e885488003d68
JammerCoder <rolandbarro@gmail.com>
2016-04-06 21:28:34 +0800

    Refactored
    problematic

---
+++ ExerciseBase/Page3.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using BookInfoCompanion;
+
+namespace ExerciseBase
+{
+    public partial class Page3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+                //Populating connection string
+                string sCnxn = ConfigurationManager.AppSettings["Cnxn"];
+
+                Books oBooks = new Books();
+                
+                DataTable dtBook = oBooks.Books()
+ 
+                this.dgBookInfo.DataSource = dtBookInfo;
+                this.dgBookInfo.DataBind();
+
+            }
+            catch (Exception ex)
+            {
+                this.lblErrorMessage.Text = ex.Message;
+            }
+
+        }
+    }
+}
--- ExerciseBase/Page3.aspx.designer.cs
+++ ExerciseBase/Page3.aspx.designer.cs
@@ -10,7 +10,7 @@
 namespace ExerciseBase {
     
     
-    public partial class Default {
+    public partial class Page3 {
         
         /// <summary>
         /// form1 control.
@@ -22,21 +22,30 @@ namespace ExerciseBase {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// hypPage2 control.
+        /// lblBookList control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink hypPage2;
+        protected global::System.Web.UI.WebControls.Label lblBookList;
         
         /// <summary>
-        /// hypPage3 control.
+        /// dgBookInfo control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink hypPage3;
+        protected global::System.Web.UI.WebControls.DataGrid dgBookInfo;
+        
+        /// <summary>
+        /// lblErrorMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblErrorMessage;
     }
 }
